1736bd77639173b7c2ba85c37c0c52da4ef3291b
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-11-09 02:34:42 +0600

    blog page redesigned

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import Blog from './Blog/Blog';
+import './Blogs.css'
 
 const Blogs = () => {
     const blogs = [
@@ -26,15 +27,26 @@ const Blogs = () => {
         },
     ]
     return (
-        <div className='py-16 lg:w-5/6 w-full mx-auto'>
-            <Helmet>
-                <title>PicMan-Blogs</title>
-            </Helmet>
+        <div className='blog-holder'>
+            <div className='breadcrumb py-24'>
+                <div className='text-center'>
+                    <h2 className='lg:text-7xl text-3xl text-white font-bold'>
+                        Blogs
+                    </h2>
+                </div>
+            </div>
+            <div className='py-16 lg:w-5/6 w-full mx-auto'>
+                <Helmet>
+                    <title>PicMan-Blogs</title>
+                </Helmet>
 
-            {
-                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
-            }
 
+
+                {
+                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
+
+            </div>
         </div>
     );
 };
