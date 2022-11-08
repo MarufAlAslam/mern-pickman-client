8758547689ddce12106c00cc798f16052bffc610
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-11-08 21:53:14 +0600

    react router dom installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
 import Main from './Pages/Main/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+        },
+        {
+          path: 'about',
+          element: <div>About</div>,
+        },
+        {
+          path: '*',
+          element: <div>You Lost Baby Girl?</div>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <Main></Main>
-    </div >
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from "react-helmet";
+import { Outlet } from 'react-router-dom';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 
@@ -7,11 +7,7 @@ const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>My Title</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
+            <Outlet></Outlet>
             <Footer></Footer>
         </div>
     );
