d2c77f46ab929e3df1dd21efd31a13b7e79b6e9a
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-11-10 19:31:34 +0600

    env variables added

--- src/Utils/Firebase/Firebase.config.js
+++ src/Utils/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCYuW20ISDtTjsJWrM3-5oyTArpaWZhQAI",
-    authDomain: "picman-a11.firebaseapp.com",
-    projectId: "picman-a11",
-    storageBucket: "picman-a11.appspot.com",
-    messagingSenderId: "611430069320",
-    appId: "1:611430069320:web:eb99c5c7f9835e8850947d"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
