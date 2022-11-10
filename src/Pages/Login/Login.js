48a4ed02ccc94b1291ed66684d7012a844c96d0f
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-11-10 22:01:52 +0600

    jwt applied

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -62,7 +62,26 @@ const Login = () => {
         popUpSignIn().then(result => {
             const user = result.user;
             console.log(user);
-            navigate(from, { replace: true })
+            const currentUser = {
+                email: user.email
+            }
+
+            console.log(currentUser)
+
+            // get jwt token
+            fetch('http://localhost:5000/jwt', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    localStorage.setItem('token', data.token)
+                    navigate(from, { replace: true })
+                })
             // setLoading(false)
         })
     }
